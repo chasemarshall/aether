ce4e10bf65913f0a5a4cf33573ff749cdfded829
chasemarshall <168988030+chasemarshall@users.noreply.github.com>
2025-08-18 22:22:55 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 // app/page.tsx
-import AppRoot from "../components/AppRoot.tsx"; // <- use a relative path
+import AppRoot from "./components/AppRoot.tsx"; // <- use a relative path
 
 export default function Page() {
   return <AppRoot />;
